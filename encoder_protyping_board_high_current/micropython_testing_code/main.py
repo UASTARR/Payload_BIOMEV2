6b0b7b96d48556dc1b90512e179a8b6055013cff
davidv2002 <31711326+davidv2002@users.noreply.github.com>
2024-05-23 11:01:46 -0600

    about half way done
    have to do some bit shifting to get things set write and then write it out still

--- encoder_protyping_board_high_current/micropython_testing_code/main.py
+++ encoder_protyping_board_high_current/micropython_testing_code/main.py
@@ -18,4 +18,35 @@
 
 from machine import Pin, I2C
 i2c = I2C(0, scl=Pin(9), sda=Pin(8), freq=10000000)
-# get the address
+
+def getAddress():
+    address = int(input("what is the hex address of the board: 0x"),16)
+    return address
+
+def getAction():
+    while True:
+        action = input("Enter n for new adress, w for write to register, q for quit: ")
+        if action in ["n", "q", "w"]:
+            return action
+        else:
+            print("Invalid entry.")
+
+def writeRegister(address):
+    register = int(input("what register: 0x"),16)
+
+
+def main():
+    address = getAddress()
+    quit = False
+    while not quit:
+        action = getAction()
+        if action == "n":
+            address = getAddress()
+        elif action == "w":
+            writeRegister(address)
+        elif action == "q":
+            quit = True
+
+    print("for your sake I hope this worked better this time")
+
+main()
