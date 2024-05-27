a58a79b603e66dcc81bc52e99cfa9ade2cea2e35
davidv2002 <31711326+davidv2002@users.noreply.github.com>
2024-05-26 21:00:18 -0600

    fixed some broken stuff and added the first complex function
    all of this is still untested and next up will be doing some real motor control code

--- encoder_protyping_board_high_current/micropython_testing_code/main.py
+++ encoder_protyping_board_high_current/micropython_testing_code/main.py
@@ -23,49 +23,53 @@ def setAddress():
     address = int(input("What is the hex address of the board: 0x"),16)
     return address
 
-def writeRegister(address, stop):
+def writeRegister(address):
     register = int(input("What register: 0x"),16)
     print("              76543210")
     value = int(input("What value: 0b"),2)
     output=register.to_bytes(1,"big")+value.to_bytes(1,"big")
-    i2c.writeto(address, output, stop)
-
-def stopBus():
-    i2c.stop()
+    i2c.writeto(address, output)
 
 def busScan():
     print(i2c.scan())
 
-def readRegister(address, stop):
+def readRegister(address):
     register = int(input("What register: 0x"),16)
-    i2c.writeto(address,register, stop)
+    i2c.writeto(address, register.to_bytes(1,"big"))
     value = i2c.readfrom(address+1,1)
+    print(value)
+
+def stepCount(address):
+    i2c.writeto(address, b"0x9f")
+    value = i2c.readfrom(address+1,4)
     print(value)
 
 def getAction():
     while True:
         action = input("Enter action:\n'scan' to scan the i2c bus\n'addr' to set i2c address\n'write' to write to a register\n"+
-                       "'read' to read from a register\n'go' to send i2c stop signal (executes current state of registers)\n'quit' to well quit\n:")
-        if action in ["addr", "quit", "write", "scan", "go", "read"]:
+                       "'read' to read from a register\n'steps' to get read the"+
+                        " number of steps taken\n'quit' to well quit\n:")
+        if action in ["addr", "quit", "write", "scan", "read", "steps"]:
             return action
         else:
             print("Invalid entry.")
 
 def main():
     quit = False
+    busScan()
+    address = setAddress()
     while not quit:
         action = getAction()
         if action == "addr":
             address = setAddress()
         elif action == "write":
-            writeRegister(address, False)
-        elif action == "go":
-            stopBus()
+            writeRegister(address)
         elif action == "read":
-            readRegister(address, False)
+            readRegister(address)
         elif action == "scan":
             busScan()
+        elif action == "steps":
+            stepCount(address)
         elif action == "quit":
+            print("For your sake I hope this worked better this time")
             quit = True
-
-    print("For your sake I hope this worked better this time")
